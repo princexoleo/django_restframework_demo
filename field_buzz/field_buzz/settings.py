c99db9bdfb20309ad5771bfc71be9f177a9d83ea
Mazharul Islam Leon <xtleon.xl@gmail.com>
2020-12-18 03:16:56 +0600

    Update settings.py
    Secret key remove from a public repository for the security purpose

--- field_buzz/field_buzz/settings.py
+++ field_buzz/field_buzz/settings.py
@@ -23,7 +23,7 @@ STATICFILES = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a3&v&c_j3sl#xp)r$0(&@_lzgw&$rlbigaj+t4xdl(h49vgq0%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
